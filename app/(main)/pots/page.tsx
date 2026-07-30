d3f79f20f092ad3f46366caafbec7890234b867e
Hongyeseo <dpseoo99@gmail.com>
2026-07-30 14:16:23 +0900

    fix(pots): allow guest users to view pots list and detail pages with guest CTA button

--- app/(main)/pots/page.tsx
+++ app/(main)/pots/page.tsx
@@ -1,7 +1,9 @@
-import { PotsView } from "@/components/pots/pots-view"
-import { getCurrentUser, listPots } from "@/lib/server-data"
+import { PotsView } from '@/components/pots/pots-view'
+import { getSessionUserOrNull, listPots } from '@/lib/server-data'
+import type { ZoneCode } from '@/lib/types'
 
 export default async function PotsPage() {
-  const [pots, me] = await Promise.all([listPots(), getCurrentUser()])
-  return <PotsView pots={pots} initialZone={me.zoneCode} />
+  const [pots, me] = await Promise.all([listPots(), getSessionUserOrNull()])
+  const initialZone: ZoneCode = me?.zoneCode ?? 'GUJEONGMUN'
+  return <PotsView pots={pots} initialZone={initialZone} />
 }
